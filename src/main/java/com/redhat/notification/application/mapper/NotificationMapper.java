348b33c7ff6cbee473a6930a802f2761dea8fab1
Ian Mesquita <ianmesquitadb@gmail.com>
2023-09-03 16:47:03 -0300

    feat: Criando o service para envio de e-mails.

--- src/main/java/com/redhat/notification/application/mapper/NotificationMapper.java
+++ src/main/java/com/redhat/notification/application/mapper/NotificationMapper.java
@@ -1,6 +1,7 @@
 package com.redhat.notification.application.mapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.redhat.notification.application.model.EmailNotification;
 import com.redhat.notification.application.model.Leed;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +15,17 @@ public class NotificationMapper {
         } catch (IOException exception) {
             throw new RuntimeException("Ocorreu erro ao converter o JSON para objeto Leed: " + exception.getMessage());
         }
+    }
+
+    public EmailNotification convertLeedToEmail(Leed leed) {
+        return EmailNotification.builder()
+                .ownerRef("Ian Mesquita - Demo")
+                .emailFrom("ian.mesquita.demo@gmail.com")
+                .emailTo(leed.getEmail())
+                .subject("Parabéns! Você foi cadastrado com sucesso")
+                .text(String.format("Olá %s, informamos que seu cadastro foi efetuado com sucesso. \n" +
+                        "Fique atento que em breve entraremos em contato pelo número %s",
+                        leed.getNome(), leed.getWhatsApp()))
+                .build();
     }
 }
--- src/main/java/com/redhat/notification/application/service/NotificationService.java
+++ src/main/java/com/redhat/notification/application/service/NotificationService.java
@@ -1,11 +1,19 @@
 package com.redhat.notification.application.service;
 
+import com.redhat.notification.application.mapper.NotificationMapper;
 import com.redhat.notification.application.model.Leed;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
+    @Autowired
+    private EmailService service;
+
+    @Autowired
+    private NotificationMapper mapper;
+
     public void processNotification(Leed leed) {
-        System.out.println(leed.toString());
+        service.sendMail(mapper.convertLeedToEmail(leed));
     }
 }
